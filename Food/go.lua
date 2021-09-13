1acb43dd4228f9d22d8acb5e0de93fe76956fe12
Jacob Alheid <shakefu@gmail.com>
2021-09-13 13:22:31 -0700

    feat: add go.lua

---
+++ Food/go.lua
@@ -0,0 +1,65 @@
+local name = "go"
+local release = "1.17"
+local version = release
+
+food = {
+    name = name,
+    description = "Open source programming language to build simple/reliable/efficient software",
+    license = "BSD-style + patent grant",
+    homepage = "https://golang.org",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://dl.google.com/go/go" .. release .. ".darwin-amd64.tar.gz",
+            sha256 = "355bd544ce08d7d484d9d7de05a71b5c6f5bc10aa4b316688c2192aeb3dacfd1",
+            resources = {
+                {
+                    path = "go/bin/go",
+                    installpath = "bin/go",
+                    executable = true
+                },
+                {
+                    path = "go/bin/gofmt",
+                    installpath = "bin/gofmt",
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://dl.google.com/go/go" .. release .. ".linux-amd64.tar.gz",
+            sha256 = "6bf89fc4f5ad763871cf7eac80a2d594492de7a818303283f1366a7f6a30372d",
+            resources = {
+                {
+                    path = "go/bin/go",
+                    installpath = "bin/go",
+                    executable = true
+                },
+                {
+                    path = "go/bin/gofmt",
+                    installpath = "bin/gofmt",
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://dl.google.com/go/go" .. release .. ".windows-amd64.zip",
+            sha256 = "2a18bd65583e221be8b9b7c2fbe3696c40f6e27c2df689bbdcc939d49651d151",
+            resources = {
+                {
+                    path = "go\\bin\\go.exe",
+                    installpath = "bin\\go.exe"
+                },
+                {
+                    path = "go\\bin\\gofmt.exe",
+                    installpath = "bin\\gofmt.exe"
+                }
+            }
+        }
+    }
+}
